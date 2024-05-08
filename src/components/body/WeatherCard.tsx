e2e902458d0e1b92cdaade20878365ca7d4b5342
Dunridge <Polinka7max@gmail.com>
2024-05-08 00:04:56 -0700

    define-cards. add output to the cards

--- src/components/body/WeatherCard.tsx
+++ src/components/body/WeatherCard.tsx
@@ -1,12 +1,15 @@
 import { IWeatherResult } from "utils/interfaces/IWeatherResult";
 
-export default function WeatherResultCard({ name }: IWeatherResult) {
+export default function WeatherResultCard({ name, coord, weather }: IWeatherResult) {
     // TODO: choose and pick the data fields from the results that you need 
 
 
     return (
-        <div className="flex inline-block rounded-md p-[22px] h-[120px] w-full bg-complementary">
-            <div className="">{ name }</div>
+        <div className="flex flex-col inline-block rounded-md p-[22px] h-fit w-full bg-complementary">
+            <div className="">City: { name }</div>
+            <div>Coordinates: (lat: <span>{coord.lat}</span>; lon: <span>{coord.lon}</span>)</div>
+            <div>Description:</div>
+            <div>{ weather.map((item) => (<div key={item.id}>{ item.main }: { item.description }</div>)) }</div>
         </div>
     );
 }
